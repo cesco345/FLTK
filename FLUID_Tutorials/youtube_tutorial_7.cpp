a8b55d9cc2f4aca97886e6f083d6f4633a045c8c
Francesco Piscani <stem.apks@gmail.com>
2018-12-01 11:51:46 -0500

    Create youtube_tutorial_7.cpp

---
+++ FLUID_Tutorials/youtube_tutorial_7.cpp
@@ -0,0 +1,44 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Widget.H>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#include "Fl_ViewerCV.h"
+
+using namespace cv;
+using namespace std;
+
+cv::Mat dst, detected_edges;
+cv::Mat src, src_gray;
+int kernel_size = 5;
+int main()
+{
+    Fl_Window * mw = new Fl_Window(900, 750, "OpenCV & FLTK Demo");
+    Fl_ViewerCV * v = new Fl_ViewerCV(100, 0, 350, 350);
+    Fl_ViewerCV * w = new Fl_ViewerCV(450,0, 350, 350);
+    Fl_ViewerCV * z = new Fl_ViewerCV(300,400, 350, 350);
+    mw->end();
+    mw->resizable(v);
+    mw->resizable(w);
+    mw->resizable(z);
+    cv::Mat src, src_gray, result;
+    cv::Mat mat=cv::imread("volks.jpeg");
+    cvtColor( mat, src_gray, CV_BGR2GRAY );
+
+    blur( src_gray, detected_edges, Size(5,5) ); 
+    v->SetImage(&mat);
+    w->SetImage(&src_gray);
+    z->SetImage(&detected_edges);
+    v->UpdateView();
+    w->UpdateView();
+    z->UpdateView();
+
+
+    mw->show();
+
+    Fl::run();
+    
+    return 0;
+}
+
